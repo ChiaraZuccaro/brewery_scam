d0e155bebc21eed53effaf9ae0ab717b15faf715
Chiara <chiara-zuc@libero.it>
2025-02-05 20:50:07 +0100

    work in progress comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WorkInProgressComponent } from './tmp/work-in-progress/work-in-progress.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'work-in-progress', component: WorkInProgressComponent },
+  { path: '**', redirectTo: '/work-in-progress' }
+];
