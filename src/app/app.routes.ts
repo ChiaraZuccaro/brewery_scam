a4ca9724fcdd7cf932ffa5c48676ac731450f970
Chiara <chiara-zuc@libero.it>
2025-02-23 20:51:23 +0100

    created homepage, added threes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { WorkInProgressComponent } from './tmp/work-in-progress/work-in-progress.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'work-in-progress', component: WorkInProgressComponent },
   { path: 'not-found', component: ErrorComponent },
   { path: '**', redirectTo: '/work-in-progress' }
