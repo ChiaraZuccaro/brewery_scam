1bac54bee1b644e46eaeb34825399c1d07111013
Chiara <chiara-zuc@libero.it>
2025-02-23 19:02:27 +0100

    work in progress img added

--- src/app/tmp/work-in-progress/work-in-progress.component.ts
+++ src/app/tmp/work-in-progress/work-in-progress.component.ts
@@ -1,31 +1,27 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AfterViewInit, Component } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
-import { BeersService } from '../../services/beers.service';
-import { ApisService } from '../../services/apis.service';
-
+import { Component } from '@angular/core';
 @Component({
-  selector: 'app-work-in-progress',
-  imports: [ ],
-  templateUrl: './work-in-progress.component.html',
-  styleUrl: './work-in-progress.component.scss'
+  selector: 'work-in-progress',
+  imports: [],
+  template: `
+  <div class="container">
+    <picture>
+      <!-- mobile -->
+      <source media="(max-width: 768px)" srcset="./assets/work_in_progress_mobile.webp">
+      <!-- desktop -->
+      <source media="(min-width: 769px)" srcset="./assets/work_in_progress.webp">
+      <!-- fallback -->
+      <img src="./assets/work_in_progress.webp" alt="work_in_progress_page">
+    </picture>
+  </div>
+  `,
+  styles: `
+  img {
+    width: 100%;
+    max-width: 100%;
+    height: 100vh;
+    max-height: 100vh;
+    object-fit: cover;
+  }
+  `
 })
-export class WorkInProgressComponent {
-  
-  constructor(private _http: HttpClient, 
-    private beersService: BeersService,
-    private apiService: ApisService
-  ) {
-    // console.log('production ????',env.isProd);
-    
-  }
-
-  public getData() {
-    this.beersService.data.set(10);
-    // const headers = new HttpHeaders({
-    //   ''
-    // })
-    
-    
-  }
-}
+export class WorkInProgressComponent {}
