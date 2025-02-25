853193fb3bfc932058c47a7f0a22553ff86076ed
Chiara <chiara-zuc@libero.it>
2025-02-25 22:53:58 +0100

    added control for webgl availability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DeviceService } from '@services/device.service';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,11 @@ export class AppComponent implements OnInit {
   private _deviceService = inject(DeviceService);
 
   ngOnInit() {
-    this._deviceService.initMediaObserver();
+    if(WebGL.isWebGL2Available()) {
+      this._deviceService.initMediaObserver();
+    } else {
+      const warning = WebGL.getWebGL2ErrorMessage();
+      document.body.appendChild(warning);
+    }
   }
 }
